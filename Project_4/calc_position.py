57c39a16a81b1d5d7008fc61dcdaafdee54858cb
Andrew Sack <andrew.sack@tufts.edu>
2018-02-12 20:15:26 -0500

    Added comments to code

--- Project_4/calc_position.py
+++ Project_4/calc_position.py
@@ -5,7 +5,9 @@ from time import sleep
 length_1 = 6.25 # (inches) length between shoulder and elbow
 length_2 = 5.5 # (inches) length between elbow and wrist ??
 sleep_time = 3 # seconds
-num_its = 5;
+num_its = 5; # number of times to run through main loop
+
+# it is assumed that the arm is fully straightened out when the program is run
 
 def main():
 	print('Calculate End Effector Position')
@@ -16,26 +18,31 @@ def main():
 	elbow = ev3.LargeMotor('outB')
 	wrist = ev3.MediumMotor('outC')
 	
+	# record position at beginning of program for zeroing motor positions
 	init_s = shoulder.position
 	init_e = elbow.position
 	init_w = wrist.position
 	
 	for  x in range(1, num_its):
-		theta = m.radians(shoulder.position - init_s)
-		alpha = theta + m.radians(elbow.position - init_e)
+		
+		# read motor position in degrees, subtract initial position to zero, then convert to radians
+		theta = m.radians(shoulder.position - init_s) # theta is shoulder position
+		alpha = theta + m.radians(elbow.position - init_e)  #alpha is theta + elbow position
 
-		ang_w = wrist.position - init_w
+		ang_w = wrist.position - init_w # get zeroed wrist position
 
-		x_pos = length_1 * m.cos(theta) + length_2 * m.cos(alpha)
+		# trig to calculate position in x and y
+		x_pos = length_1 * m.cos(theta) + length_2 * m.cos(alpha) 
 		y_pos = length_1 * m.sin(theta) + length_2 * m.sin(alpha)
 		
+		# print position to console
 		print('X Position: ' + str(x_pos) + ' inches')
 		print('Y Position: ' + str(y_pos) + ' inches')
 		print('End Effector: ' + str(ang_w) + ' degrees')
 
-		sleep(sleep_time)
+		sleep(sleep_time) 
 
-	# end while
+	# end for loop
 
 if __name__ == '__main__':
 	main()
--- Project_4/record_and_play.py
+++ Project_4/record_and_play.py
@@ -3,11 +3,13 @@ import math as m
 from time import sleep
 # parameters
 length_1 = 6.25 # (inches) length between shoulder and elbow
-length_2 = 5.5 # (inches) length between elbow and wrist ??
+length_2 = 5.5 # (inches) length between elbow and wrist 
 
-sleep_time = 0.1 # seconds
-num_its = 150 
-speed = 300
+sleep_time = 0.1 # seconds between points collected and played
+num_its = 150 # number of data points to collect for arm position. num_its * sleep_time is the length of time data is collected for
+speed = 300 # speed to run motors at for playback
+
+# it is assumed that the arm is fully straightened out when the program is run
 
 def main():
 	print('Calculate End Effector Position')
@@ -17,32 +19,39 @@ def main():
 	shoulder = ev3.LargeMotor('outA')
 	elbow = ev3.LargeMotor('outB')
 	wrist = ev3.MediumMotor('outC')
-	#NEED CODE button = ev3.
+	#NEED CODE button = ev3.Button('in1')
 	
 	# record
 
+	# record position at beginning of program for zeroing motor positions
 	init_ang_s = shoulder.position
 	init_ang_e = elbow.position
 	init_ang_w = wrist.position
 	
+	# set previous angle values to initial at beginning
 	prev_s = init_s
 	prev_e = init_e
 	prev_w = init_w
 	
+	# initialize empty lists for relative positions for each motor
 	s_list = []
 	e_list = []
 	w_list = []
 	
-	for  x in range(1, num_its):
+	# recording loop
+	for  x in range(1, num_its): 
 		
+		# get motor positions
 		curr_s = shoulder.position
 		curr_e = elbow.position
 		curr_w = wrist.position
 
+		# save position relative to previous position
 		s_list.append(curr_s - prev_s)
 		e_list.append(curr_e - prev_e)
 		w_list.append(curr_w - prev_w)
 		
+		# previous positions are now current
 		prev_s = curr_s
 		prev_e = curr_e
 		prev_w = curr_w
@@ -50,42 +59,35 @@ def main():
 		sleep(sleep_time)
 
 	# end for loop
-
-	# print('s')
-	# print(s_list)
-	# print('e')
-	# print(e_list)
-	# print('w')
-	# print(w_list)
-	# print('end')
 	
-
-	# playback
-
-if __name__ == '__main__':
-	main()
+	# code idles while user resets arm position
+	while !(button.is_pressed()):
+		# Wait until button is pressed
+	# end while
 
 
+	# playback loop
+	for x in range(0, len(s_list)): # go through angle lists
+		
+		# get current relative angles from list
+		shoulder_ang = s_list[x]
+		elbow_ang = e_list[x]
+		wrist_ang = w_list[x]
 	
-	init_ang_s = shoulder.position
-	init_ang_e = elbow.position
-	init_ang_w = wrist.position
-	
-	#print(shoulder.position)
-	#print(elbow.position)
-	#print(wrist.position)
-	
-	for x in range(0, len(target_s)):
-		shoulder_ang = target_s[x]
-		elbow_ang = target_e[x]
-		wrist_ang = target_w[x]
-	
+		# set motors to move by those angles at given speed
 		shoulder.run_to_rel_pos(position_sp=shoulder_ang, speed_sp=speed, stop_action="coast")
 		elbow.run_to_rel_pos(position_sp=elbow_ang, speed_sp=speed, stop_action="coast")
 		wrist.run_to_rel_pos(position_sp=wrist_ang, speed_sp=speed, stop_action="coast")
-		sleep(0.1)
-	# end while
+		
+		sleep(sleep_time)
 	
+	# end for loop
+
 
 if __name__ == '__main__':
 	main()
+
+
+	
+
+	
